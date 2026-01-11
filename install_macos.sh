6b994c79d41fc9a73c475cde2551e95e54a7554a
Shawn Kearney <shawnpk29@me.com>
2026-01-11 16:33:51 -0700

    Add install script for MacOS; change file name for Ubuntu install

---
+++ install_macos.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# this file is for installing dotfiles on macOS using yadm
+if [ -z "$USER" ]; then
+  USER=$(id -un)
+fi
+
+cd $HOME
+
+# Install yadm for managing dotfiles
+brew install yadm
+
+yadm clone -f https://github.com/shawnpk/dotfiles.git
+yadm sparse-checkout set --no-cone '/*' '!README.md' '!LICENSE'
+yadm remote set-url origin git@github.com:shawnpk/dotfiles.git
+
+# Install LazyGit
+brew install lazygit
+
+# Install treesitter
+npm install -g tree-sitter
+
+rm -rf dotfiles
